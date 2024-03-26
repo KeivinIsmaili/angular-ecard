d3a052aaf8e90ee66c60734fc98f91565da12876
Keivin Ismaili <keivin.ismaili@fti.edu.al>
2024-03-27 00:59:21 +0100

    Added SplitButton, Toolbar, InputText Modules and removed Search Component

--- src/app/header/top-menu/top-menu.module.ts
+++ src/app/header/top-menu/top-menu.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TopMenuComponent } from './top-menu.component';
-import { SearchModule } from './search/search.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
 import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
     declarations: [
@@ -17,11 +18,12 @@ import { SplitButtonModule } from 'primeng/splitbutton';
         BrowserModule,
         FormsModule,
         CommonModule,
-        SearchModule,
         FontAwesomeModule,
         ButtonModule,
         BadgeModule,
-        SplitButtonModule
+        SplitButtonModule,
+        ToolbarModule,
+        InputTextModule
     ],
     exports: [
         TopMenuComponent
